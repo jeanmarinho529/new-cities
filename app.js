0ad198f97d906f3bed33e324f5f57be027461af2
jean marinho <jeanmarinho529@gmail.com>
2022-06-16 14:03:25 -0300

    adjusting code indentation

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const apiUrl = 'https://api.teleport.org/api/'
 const apiSearch = 'countries/'
 
-function urlBuilder(){
-    const finaLink = apiUrl+apiSearch;
+function urlBuilder() {
+    const finaLink = apiUrl + apiSearch;
     return finaLink
 }
 
 async function apiGet() {
-    let response =  await fetch(urlBuilder());
+    let response = await fetch(urlBuilder());
     let data = await response.json()
     let countryData = data._links;
     let countriesList = countryData['country:items']
     //console.log(countriesList[2])
-    for (country in countriesList){
+    for (country in countriesList) {
         console.log(countriesList[country]);
-   }
+    }
 }
 
 apiGet();
@@ -24,7 +24,7 @@ apiGet();
 
 const list = document.querySelectorAll('.list');
 
-function activeLink (){
+function activeLink() {
     list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
 }
@@ -35,13 +35,13 @@ list.forEach((item) => item.addEventListener('click', activeLink))
 
 const formControlsElements = document.querySelectorAll('.form-control')
 
-for(let control of formControlsElements) {
+for (let control of formControlsElements) {
 
     const controlInputElement = control.children[1]
 
     controlInputElement.addEventListener('keyup', event => {
 
-        if(event.target.checkValidity()) {
+        if (event.target.checkValidity()) {
 
             control.classList.remove('error')
 
