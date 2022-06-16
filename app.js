64dadc5b89aba0f806b156a8dfb84b38aa8d9b76
lucas <lucas@pop-os.localdomain>
2022-06-15 22:52:05 -0300

    html e css

--- app.js
+++ app.js
@@ -18,3 +18,39 @@ async function apiGet() {
 }
 
 apiGet();
+
+
+// Control Form
+
+const list = document.querySelectorAll('.list');
+
+function activeLink (){
+    list.forEach((item) => item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+
+list.forEach((item) => item.addEventListener('click', activeLink))
+
+
+const formControlsElements = document.querySelectorAll('.form-control')
+
+for(let control of formControlsElements) {
+
+    const controlInputElement = control.children[1]
+
+    controlInputElement.addEventListener('keyup', event => {
+
+        if(event.target.checkValidity()) {
+
+            control.classList.remove('error')
+
+        } else {
+
+            control.classList.add('error')
+
+        }
+
+    })
+
+}
